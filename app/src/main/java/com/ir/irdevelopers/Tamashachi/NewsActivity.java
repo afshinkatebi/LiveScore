9703d6f29a1957d969501b2b2339cfde565a7613
afshinkatebi <afshinkatebi@gmail.com>
2016-06-29 08:54:13 +0430

    news

--- app/src/main/java/com/ir/irdevelopers/Tamashachi/NewsActivity.java
+++ app/src/main/java/com/ir/irdevelopers/Tamashachi/NewsActivity.java
@@ -1,9 +1,12 @@
 package com.ir.irdevelopers.Tamashachi;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class NewsActivity extends AppCompatActivity {
 
@@ -11,6 +14,13 @@ public class NewsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+
     }
 
     public void ShareOnClick(View view) {
